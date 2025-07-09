022dccd5d4379953100a89968b2def913086d2ce
slendchat <slendchat@gmail.com>
2025-07-09 19:30:56 +0000

    update board dimensions and improve cell initialization logic

--- inc/board.h
+++ inc/board.h
@@ -1,8 +1,8 @@
 #pragma once
 #include "../inc/cell.h"
 
-#define height_y 400
-#define width_x 700
+#define height_y 450
+#define width_x 1250
 
 extern cell_entity main_board[height_y][width_x];
 extern cell_entity buffer_board[height_y][width_x];
--- src/main.c
+++ src/main.c
@@ -12,30 +12,30 @@
 
 int main()
 {
-  
+  // init plain board
   init_board();
 
-  // Initialize the board with some alive cells
+  // Generate some alive cells on the board
   srand(time(NULL));
   for (int y = 1; y < height_y - 1; y++) {
     for (int x = 1; x < width_x - 1; x++) {
-        int noise = (rand() % 100) < 2; 
-        int alive_neighbors = 0;
+      int noise = (rand() % 100) < 2; 
+      int alive_neighbors = 0;
 
         for (int dy = -1; dy <= 1; dy++) {
-            for (int dx = -1; dx <= 1; dx++) {
-                if (dy == 0 && dx == 0) continue;
-                if (main_board[y + dy][x + dx].cell_type == ALIVE) {
-                    alive_neighbors++;
-                }
-            }
+          for (int dx = -1; dx <= 1; dx++) {
+            if (dy == 0 && dx == 0) continue;
+              if (main_board[y + dy][x + dx].cell_type == ALIVE) {
+                alive_neighbors++;
+              }
+          }
         }
 
-        int clustering = (alive_neighbors > 0 && alive_neighbors <= 2 && (rand() % 100) < 25);
+      int clustering = (alive_neighbors > 0 && alive_neighbors <= 2 && (rand() % 100) < 25);
 
-        if (noise || clustering) {
-            cell_revive(&main_board[y][x]);
-        }
+      if (noise || clustering) {
+        cell_revive(&main_board[y][x]);
+      }
     }
   }
 
@@ -50,8 +50,6 @@ int main()
     
     sleep_ms(50);
 
-    // if (changed_cells == 0)
-    //   break;
   }
 
   return 0;
